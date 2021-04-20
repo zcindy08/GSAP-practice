e91047b7d95ae329c2bd2e078ebaef8e9277507d
zcindy08 <63681878+zcindy08@users.noreply.github.com>
2021-04-20 19:08:58 -0400

    Practice for April 20

---
+++ dev/js/demo.js
@@ -0,0 +1,25 @@
+import $ from "jquery";
+import {gsap} from "gsap";
+
+$(document).ready(function(){
+
+  //var tl = gsap.timeline();
+  var redBox = $('.red-box');
+  var blueBox = $('.blue-box');
+  var orangeBox = $('.orange-box');
+  var boxArray = [redBox, blueBox, orangeBox];
+
+  //Wrap Utility
+  //var color = gsap.utils.wrap([]);
+
+  console.log('Page loaded');
+
+
+
+
+
+
+
+
+
+});//ready END
